8eb23acd266f34c26aafe05dc91672c243bc5fb6
Sofie Finnes <sofie+git@finnes.dev>
2026-03-12 11:16:28 +0100

    release: v1.0.1

--- mielelogic_api/_version.py
+++ mielelogic_api/_version.py
@@ -5,4 +5,4 @@ from importlib.metadata import PackageNotFoundError, version
 try:
     __version__ = version("mielelogic-api")
 except PackageNotFoundError:
-    __version__ = "1.0.0"
+    __version__ = "1.0.1"
